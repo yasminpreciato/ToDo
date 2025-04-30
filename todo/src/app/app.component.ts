d91b63ebd8f35d19068cbe58ee0eac4412122890
yasminpreciato <yasminfpreciato2019@gmail>
2025-04-29 22:12:26 -0300

    criado Angular

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,13 +1,49 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { TodoListComponent } from './todo-list/todo-list.component';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, TodoListComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: 'app-todo-list',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './todo-list.component.html',
+  styleUrl: './todo-list.component.css'
 })
-export class AppComponent {
-  title = 'todo';
+export class TodoListComponent implements OnInit {
+
+  taskArray: { taskName: string, isCompleted: boolean }[] = [];
+
+  ngOnInit(): void {
+    const savedTasks = localStorage.getItem('tasks');
+    if (savedTasks) {
+      this.taskArray = JSON.parse(savedTasks);
+    }
+  }
+
+  saveToLocalStorage(){
+    localStorage.setItem('tasks', JSON.stringify(this.taskArray));
+  }
+
+  onSubmit(form: NgForm) {
+    //console.log(form);
+
+    this.taskArray.push({
+      taskName: form.controls['task'].value,
+      isCompleted: false
+    });
+
+    this.saveToLocalStorage();
+    form.reset();
+  }
+
+  onDelete(index: number) {
+    this.taskArray.splice(index, 1);
+    this.saveToLocalStorage();
+  }
+
+  oncheck(index: number) {
+    this.taskArray[index].isCompleted = !this.taskArray[index].isCompleted;
+    this.saveToLocalStorage();
+  }
+
 }
